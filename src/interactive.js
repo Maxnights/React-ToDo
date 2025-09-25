4c14fd9f6de7f43a3ab0b8f5224e8f790b14cacd
Maxim Somov <109459573+Maxnights@users.noreply.github.com>
2025-09-26 00:31:57 +0400

    Update interactive.js

--- src/interactive.js
+++ src/interactive.js
@@ -1,40 +1,13 @@
-import display from './methods.js';
+import Display from './methods.js';
 
 export default class Interactive {
-    static changeCompletedListCheck = (statusCheck, id) => {
-      const toDoLists = display.getToDoListFromStorage();
-      toDoLists[id].completed = statusCheck;
-      display.addListToStorage(toDoLists);
-      display.showLists();
-    }
+  static toggleCompleted(index, completed) {
+    Display.update(index, { completed });
+    window.dispatchEvent(new CustomEvent('todos:updated'));
+  }
 
-    // checkbox status
-    static checkStatusEvent = () => (
-      document.querySelectorAll('.checkbox').forEach((checkbox) => checkbox.addEventListener('change', () => {
-        let statusCheck;
-        let id;
-        if (checkbox.id > 0) {
-          id = checkbox.id - 1;
-        } else {
-          id = 0;
-        }
-
-        if (checkbox.checked === true) {
-          statusCheck = true;
-        } else if (checkbox.checked !== true) {
-          statusCheck = false;
-        }
-
-        this.changeCompletedListCheck(statusCheck, id);
-      }))
-    )
-
-    static clearCompletedToDoLists = () => {
-      let toDoLists = display.getToDoListFromStorage();
-
-      toDoLists = toDoLists.filter((item) => item.completed !== true);
-      display.newIndexNum(toDoLists);
-      display.addListToStorage(toDoLists);
-      display.showLists();
-    }
+  static clearCompleted() {
+    Display.clearCompleted();
+    window.dispatchEvent(new CustomEvent('todos:updated'));
+  }
 }
